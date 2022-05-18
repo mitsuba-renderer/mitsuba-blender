157ec995cce8075e758e8bfc7f774101199b72af
Dorian Ros <dorian.ros@gmail.com>
2022-05-18 17:02:42 +0200

    Change Blender addon dir discovery method

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -8,19 +8,26 @@ class SetupPlugin:
     def __init__(self):
         mi_addon_root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         self.mi_addon_dir = os.path.join(mi_addon_root_dir, 'mitsuba2-blender')
-        self.bl_addon_dir = os.path.join(bpy.utils.user_resource('SCRIPTS', 'addons'), 'mitsuba2-blender')
+        bl_script_dirs = bpy.utils.script_path(use_user=True)
+        self.bl_script_dir = None
+        for dir in bl_script_dirs:
+            if dir.endswith('scripts'):
+                self.bl_script_dir = dir
+        if self.bl_script_dir is None:
+            raise RuntimeError('Cannot resolve Blender script directory')
+        self.bl_addon_dir = os.path.join(self.bl_script_dir, 'addons')
+        self.bl_mi_addon_dir = os.path.join(self.bl_addon_dir, 'mitsuba2-blender')
         sys.path.append(self.mi_addon_dir)
 
     def pytest_configure(self, config):
-        if os.path.exists(self.bl_addon_dir):
-            os.remove(self.bl_addon_dir)
+        if os.path.exists(self.bl_mi_addon_dir):
+            os.remove(self.bl_mi_addon_dir)
         
         if sys.platform == 'win32':
             import _winapi
-            _winapi.CreateJunction(str(self.mi_addon_dir), str(self.bl_addon_dir))
+            _winapi.CreateJunction(str(self.mi_addon_dir), str(self.bl_mi_addon_dir))
         else:
-            os.makedirs(bpy.utils.user_resource('SCRIPTS', 'addons'), exist_ok=True)
-            os.symlink(self.mi_addon_dir, self.bl_addon_dir, target_is_directory=True)
+            os.symlink(self.mi_addon_dir, self.bl_mi_addon_dir, target_is_directory=True)
         
         bpy.ops.preferences.addon_enable(module='mitsuba2-blender')
         bpy.context.preferences.addons['mitsuba2-blender'].preferences.mitsuba_path = "C:\\Users\\Dorian\\Documents\\EPFL\\sp-blender-addon\\mitsuba3\\build\\Release"
@@ -28,7 +35,7 @@ class SetupPlugin:
     def pytest_unconfigure(self):
         bpy.ops.preferences.addon_disable(module='mitsuba2-blender')
         
-        os.remove(self.bl_addon_dir)
+        os.remove(self.bl_mi_addon_dir)
 
 if __name__ == '__main__':
     pytest_args = ["tests"]
