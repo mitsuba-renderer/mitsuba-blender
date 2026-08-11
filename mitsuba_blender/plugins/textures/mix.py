8a3900e06c7a4a7f39712ef10473c512bf76afe1
arthur herbette <arthur.herbette@epfl.ch>
2026-08-11 14:45:16 +0200

    add eval_1_grad into mix texture plugin

--- mitsuba_blender/plugins/textures/mix.py
+++ mitsuba_blender/plugins/textures/mix.py
@@ -81,12 +81,151 @@ def register(mi, dr):
 
             return mi.Color3f(self._process(si, val_a, val_b, active))
 
-
         def mean(self):
             return 0.5
 
         def resolution(self):
             return self.a.resolution
+
+        def eval_1_grad(self, si, active=True):
+            a = self.a.eval_1(si, active)
+            b = self.b.eval_1(si, active)
+            t = self.factor.eval_1(si, active)
+
+            # Spatial gradients (du, dv)
+            grad_a = self.a.eval_1_grad(si, active)
+            grad_b = self.b.eval_1_grad(si, active)
+            grad_t = self.factor.eval_1_grad(si, active)
+
+            # Handle clamp_factor
+            if self.clamp_factor:
+                raw_t = t
+                t = dr.clip(raw_t, 0.0, 1.0)
+
+                # d clip(t)/dt
+                t_mask = (raw_t > 0.0) & (raw_t < 1.0)
+                grad_t = dr.select(t_mask, grad_t, mi.Vector2f(0.0))
+
+            da, db, dt = self._blend_grad(
+                self.blend_type, a, b, t
+            )
+
+            # Chain rule
+            grad = da * grad_a + db * grad_b + dt * grad_t
+
+            # Handle clamp_result
+            if self.clamp_result:
+                result = self._blend(self.blend_type, a, b, t)
+
+                # d clip(result)/d result
+                result_mask = (result > 0.0) & (result < 1.0)
+                grad = dr.select(
+                    result_mask,
+                    grad,
+                    mi.Vector2f(0.0)
+                )
+
+            return mi.Vector2f(grad)
+
+
+        def _blend_grad(self, blend_type, a, b, t):
+            facm = 1.0 - t
+
+            if blend_type == 'MIX':
+                # r = (1-t)a + tb
+                da = facm
+                db = t
+                dt = b - a
+
+            elif blend_type == 'ADD':
+                # r = a + tb
+                da = 1.0
+                db = t
+                dt = b
+
+            elif blend_type == 'MULTIPLY':
+                # r = a(1-t+tb)
+                da = facm + t * b
+                db = t * a
+                dt = a * (b - 1.0)
+
+            elif blend_type == 'SUBTRACT':
+                # r = a - tb
+                da = 1.0
+                db = -t
+                dt = -b
+
+            elif blend_type == 'SCREEN':
+                # r = 1 - (1-tb)(1-a)
+                da = 1.0 - t * b
+                db = t * (1.0 - a)
+                dt = b * (1.0 - a)
+
+            elif blend_type == 'DIVIDE':
+                # r = a(1-t) + t*a/b, with r=a when b=0
+                valid = b != 0.0
+                bsafe = dr.select(valid, b, 1.0)
+
+                da_valid = facm + t / bsafe
+                db_valid = -t * a / (bsafe * bsafe)
+                dt_valid = a / bsafe - a
+
+                da = dr.select(valid, da_valid, 1.0)
+                db = dr.select(valid, db_valid, 0.0)
+                dt = dr.select(valid, dt_valid, 0.0)
+
+            elif blend_type == 'DIFFERENCE':
+                # r = (1-t)a + t|a-b|
+                s = dr.sign(a - b)
+
+                da = facm + t * s
+                db = -t * s
+                dt = dr.abs(a - b)
+
+            elif blend_type == 'DARKEN':
+                # r = lerp(a, min(a,b), t)
+                m = dr.minimum(a, b)
+
+                da_min = dr.select(a <= b, 1.0, 0.0)
+                db_min = dr.select(b < a, 1.0, 0.0)
+
+                da = facm + t * da_min
+                db = t * db_min
+                dt = m - a
+
+            elif blend_type == 'LIGHTEN':
+                # r = max(a, tb)
+                x = t * b
+                use_x = x > a
+
+                da = dr.select(use_x, 0.0, 1.0)
+                db = dr.select(use_x, t, 0.0)
+                dt = dr.select(use_x, b, 0.0)
+
+            elif blend_type == 'OVERLAY':
+                # lo = a * (1-t + 2tb)
+                # hi = 1 - (1-t + 2t(1-b)) * (1-a)
+
+                lo_da = facm + 2.0 * t * b
+                lo_db = 2.0 * t * a
+                lo_dt = a * (2.0 * b - 1.0)
+
+                hi_da = facm + 2.0 * t * (1.0 - b)
+                hi_db = 2.0 * t * (1.0 - a)
+                hi_dt = (1.0 - 2.0 * b) * (1.0 - a)
+
+                use_lo = a < 0.5
+
+                da = dr.select(use_lo, lo_da, hi_da)
+                db = dr.select(use_lo, lo_db, hi_db)
+                dt = dr.select(use_lo, lo_dt, hi_dt)
+
+            else:
+                raise Exception(
+                    f'Mix blend type "{blend_type}" is not supported'
+                )
+
+            return da, db, dt
     mi.register_texture('mix', Mix)
 
 
